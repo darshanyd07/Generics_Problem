c48f658cf134efddb9905cf654adbdb63a8aebaf
champ09D <darshanmhatre709@gmail.com>
2022-07-07 14:54:37 +0530

    Extend_the_max_method_to_also_print File Done

--- src/generics/MaximumProblem.java
+++ src/generics/MaximumProblem.java
@@ -2,26 +2,25 @@ package generics;
 
 import java.util.Scanner;
 
-public class MaximumProblem
-{
+public class MaximumProblem {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Integer Int1 = 10 , Int2 = 12, Int3 = 7;
+        Float float1 = 2.7f , float2 = 19.3f, float3 = 7.5f;
+        String xString = "Cherry" , yString = "Banana", zString = "Pineapple";
 
-        Operation operation = new Operation();
+        Operation operation = new Operation(Int1,Int2,Int3);
+        int intMax=operation.testMaximum(Int1,Int2,Int3);
+        operation.printMax(Int1, Int2, Int3,intMax);
 
-        operation.getInputForInteger();
-        int integerMax =operation.testMaximum(operation.firstNumber,operation.secondNumber,operation.thirdNumber);
-        System.out.println(integerMax+" is maximum amongst integer numbers");
+        Operation operation1 = new Operation(float1,float2,float3);
+        Float floatMax =operation1.testMaximum(float1,float2,float3);
+        operation.printMax(float1, float2, float3,floatMax);
 
-        operation.getInputForFloat();
-        Float floatMax =operation.testMaximum(operation.number1,operation.number2,operation.number3);
-        System.out.println(floatMax+" is maximum amongst float numbers");
 
-        operation.getInputForString();
-        String stringMax =operation.testMaximum(operation.string1,operation.string2,operation.string3);
-        System.out.println(stringMax+" is maximum alphabetically amongst strings");
+        Operation operation2 = new Operation(xString,yString,zString);
+        String stringMax =operation2.testMaximum(xString,yString,zString);
+        operation.printMax(xString, yString, zString,stringMax);
 
     }
-
 }
--- src/generics/Operation.java
+++ src/generics/Operation.java
@@ -4,45 +4,21 @@ import java.util.Scanner;
 
 import java.util.Scanner;
 
-public class Operation <E>
+public class Operation <E extends Comparable<E>>
 {
-    public static Integer firstNumber,secondNumber,thirdNumber;
-    public static Float number1=0.0f,number2=0.0f,number3=0.0f,max2=0.0f;
-    public static String string1,string2,string3,max3;
+    E firstValue,secondValue,thirdValue;
 
-    static Scanner sc = new Scanner(System.in);
-
-    public static void getInputForInteger()
+    public Operation(E firstValue, E secondValue, E thirdValue)
     {
-        System.out.println("Enter first integer Number");
-        firstNumber = sc.nextInt();
-        System.out.println("Enter second integer Number");
-        secondNumber = sc.nextInt();
-        System.out.println("Enter third integer Number");
-        thirdNumber = sc.nextInt();
-    }
-    public static void getInputForFloat()
-    {
-        System.out.println("Enter first float Number");
-        number1 = sc.nextFloat();
-        System.out.println("Enter second float Number");
-        number2 = sc.nextFloat();
-        System.out.println("Enter third float Number");
-        number1 = sc.nextFloat();
-    }
-    public static void getInputForString()
-    {
-        System.out.println("Enter first string");
-        string1 = sc.next();
-        System.out.println("Enter second string");
-        string2 = sc.next();
-        System.out.println("Enter third string");
-        string3 = sc.next();
+        super();
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
     }
 
     public static <E extends Comparable<E>> E testMaximum(E a,E b,E c)
     {
-        E max= a;
+        E max = a;
         if(b.compareTo(max) > 0){
             max = b;
         }
@@ -51,4 +27,9 @@ public class Operation <E>
         }
         return max;
     }
+    public static <E> void printMax(E a, E b, E c,E max)
+    {
+        System.out.println("Maximum of "+a+" , "+b+" , "+c+" is "+max);
+    }
 }
+
