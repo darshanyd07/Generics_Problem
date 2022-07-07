e8991995c0d1d31305826c1200933dd44751978f
champ09D <darshanmhatre709@gmail.com>
2022-07-07 12:04:20 +0530

    Maximum_Problem_INT_Float  File Done

--- src/generics/MaximumProblem.java
+++ src/generics/MaximumProblem.java
@@ -1,36 +1,22 @@
 package generics;
+
 import java.util.Scanner;
 
 public class MaximumProblem
 {
-    static Integer firstNumber,secondNumber,thirdNumber;
 
     public static void main(String[] darsh)
     {
-        getInput();
-        testMaximum(firstNumber,secondNumber,thirdNumber);
-
-    }
-    public static void getInput() {
-        System.out.println("----------Find Maximum Problem using Generics---------");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first Number");
-        firstNumber = sc.nextInt();
-        System.out.println("Enter second Number");
-        secondNumber = sc.nextInt();
-        System.out.println("Enter third Number");
-        thirdNumber = sc.nextInt();
-    }
-    public static void testMaximum(Integer x,Integer y,Integer z)
-    {
-        Integer max = x;
-        if(y.compareTo(max) > 0){
-            max = y;
-        }
-        if(z.compareTo(max) > 0){
-            max = z;
-        }
-        System.out.println(max+" is maximum number");
+        System.out.println("---------Find Maximum Problem using Generics-----------");
+        Operation operation = new Operation();
+        operation.getInputForInteger();
+        operation.testMaximumInteger(operation.firstNumber,operation.secondNumber,operation.thirdNumber);
+        System.out.println(operation.max1+" is maximum amongst integer numbers");
+        System.out.println("-----------------------------");
+        operation.getInputForFloat();
+        operation.testMaximumFloat(operation.number1,operation.number2,operation.number3);
+        System.out.println(operation.max2+" is maximum amongst float numbers");
         System.out.println("-----------------------------");
     }
+
 }
